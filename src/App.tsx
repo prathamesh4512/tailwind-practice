15746d04d1d8d987cc5911c90fb5c7451b98ec96
prathamesh4512 <prathamesh4512@gmail.com>
2024-12-25 23:49:14 +0530

    customize tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,25 @@ function App() {
 
   return (
     <div>
+      <ul>
+        <li>h1</li>
+        <li>h2</li>
+        <li>h3</li>
+
+      </ul>
       <PseudoClasses/>
+      <div className="max-sm:text-red-400">
+        hello
+      </div>
+      <p className="dark:text-pink-400 text-red-400 ">
+      dark
+      </p>
+      <a href="hello">Hello Anchor</a>
+
+      <div className="flex-row-center">
+      <p>h1</p>
+      <p>h2</p>
+      </div>
     </div>
   )
 }
